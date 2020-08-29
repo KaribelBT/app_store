276f2be5a424a1369e27480c112bd8c50a17d9a0
KaribelBT <karibel.barco@gmail.com>
2020-08-29 18:43:16 -0300

    register form

--- front/src/components/Register.js
+++ front/src/components/Register.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 
-class Login extends Component {
-    constructor(props){
+class Register extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            email:null,
-            password:null
+        this.state = {
+            email: null,
+            password: null,
+            isDev: false
         }
     }
-    handleEmail = (event) =>{
+    handleEmail = (event) => {
         this.setState({
-            email:event.target.value
+            email: event.target.value
         })
     }
-    handlePassword = (event) =>{
+    handlePassword = (event) => {
         this.setState({
-            password:event.target.value
+            password: event.target.value
+        })
+    }
+    handleIsDev = (event) => {
+        this.setState ({
+            isDev: event.target.checked
         })
     }
     handleSubmit = async (event) => {
         event.preventDefault()
-        let result = await fetch(`http://localhost:3001/api/users/login`, {
+        let result = await fetch(`http://localhost:3001/api/users/`, {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(this.state)
@@ -34,25 +40,25 @@ class Login extends Component {
                 <div className="col-lg-12">
                     <div className="row">
                         <div className="col-lg-12">
-                            <form onSubmit={this.handleSubmit} id="login-form">
+                            <form onSubmit={this.handleSubmit} id="register-form">
                                 <div className="form-group">
                                     <input onChange={this.handleEmail} type="text" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email" />
-                                </div>
+								</div>
                                 <div className="form-group">
                                     <input onChange={this.handlePassword} type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
-                                </div>
+								</div>
                                 <div className="form-group text-center">
-                                    <input type="checkbox" tabIndex="3" className="" name="remember" id="remember" />
-                                    <label htmlFor="remember"> Remember Me</label>
+                                    <input onChange={this.handleIsDev} type="checkbox" tabIndex="3" className="" name="isDev" id="isDev" />
+                                    <label htmlFor="isDev"> Click here if you are a developer</label>
                                 </div>
                                 <div className="form-group">
                                     <div className="row">
                                         <div className="col-sm-12">
-                                            <input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Log In" />
-                                        </div>
+                                             <input type="submit" name="register-submit" id="register-submit" tabIndex="4" className="form-control btn btn-register" value="Register Now" />
+										</div>
                                     </div>
                                 </div>
-                            </form>
+							</form>
                         </div>
                     </div>
                 </div>
@@ -61,5 +67,4 @@ class Login extends Component {
     }
 }
 
-
-export default Login;
+export default Register;
